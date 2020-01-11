7123dd593a5421fdae8376de4add93f469959813
Ahmad Raza <ahmad.raza@zameen.com>
2020-01-12 02:35:40 +0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'hotels-hear-me';
-}
+import { Component } from '@angular/core';  
+declare var H: any;  
+  
+@Component({  
+  selector: 'app-root',  
+  templateUrl: './app.component.html',  
+  styleUrls: ['./app.component.css']  
+})  
+export class AppComponent {  
+  title = 'HereMapDemo';
+} 
